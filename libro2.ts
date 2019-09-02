3564e2d579e959cd953179301dcb7531ece16f7f
ZarelyNL <zarely_navarrete@hotmail.com>
2019-09-01 23:39:09 -0600

    gener

--- libro2.ts
+++ libro2.ts
@@ -1,23 +1,11 @@
-function loadItem(id) {
-    return new Promise((resolve) => {
-    console.log('loading item', id);
-    setTimeout(() => {
-    resolve({ id: id });
-    }, 1000);
-    });
+function* infiniteSequence() {
+    var i = 0;
+    while(true) {
+    yield i++;
     }
-    let item1, item2;
-    loadItem(1)
-    .then((res) => {
-    item1 = res;
-    return loadItem(2);
-    })
-    .then((res) => {
-    item2 = res;
-    console.log('done');
-    });
-    Promise.all([loadItem(1), loadItem(2)])
-    .then((res) => {
-    [item1, item2] = res;
-    console.log('done');
-    });
+}
+    var iterator = infiniteSequence();
+    while (true) {
+    console.log(iterator.next());
+    }
+    
