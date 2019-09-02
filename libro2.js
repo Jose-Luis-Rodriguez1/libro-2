498921153ff341944b4d51fca101c17785f520ac
ZarelyNL <zarely_navarrete@hotmail.com>
2019-09-01 23:54:06 -0600

    herencia3

--- libro2.js
+++ libro2.js
@@ -1,29 +1,63 @@
-var __extends = (this && this.__extends) || function (d, b) {
-    for (var p in b)
-        if (b.hasOwnProperty(p))
-            d[p] = b[p];
-    function __() { this.constructor = d; }
-    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-};
-var Point = (function () {
-    function Point(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-    Point.prototype.add = function (point) {
-        return new Point(this.x + point.x, this.y + point.y);
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
     };
-    return Point;
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Animal = /** @class */ (function () {
+    function Animal(theName) {
+        this.name = theName;
+    }
+    Animal.prototype.move = function (distanceInMeters) {
+        if (distanceInMeters === void 0) { distanceInMeters = 0; }
+        console.log(this.name + " moved " + distanceInMeters + "m.");
+    };
+    return Animal;
 }());
-var Point3D = (function (_super) {
-    __extends(Point3D, _super);
-    function Point3D(x, y, z) {
-        _super.call(this, x, y);
-        this.z = z;
+var Snake = /** @class */ (function (_super) {
+    __extends(Snake, _super);
+    function Snake(name) {
+        return _super.call(this, name) || this;
     }
-    Point3D.prototype.add = function (point) {
-        var point2D = _super.prototype.add.call(this, point);
-        return new Point3D(point2D.x, point2D.y, this.z + point.z);
+    Snake.prototype.move = function (distanceInMeters) {
+        if (distanceInMeters === void 0) { distanceInMeters = 5; }
+        console.log("Slithering...");
+        _super.prototype.move.call(this, distanceInMeters);
     };
-    return Point3D;
-}(Point));
+    return Snake;
+}(Animal));
+var Rhino = /** @class */ (function (_super) {
+    __extends(Rhino, _super);
+    function Rhino(name) {
+        return _super.call(this, name) || this;
+    }
+    Rhino.prototype.move = function (distanceInMeters) {
+        if (distanceInMeters === void 0) { distanceInMeters = 10; }
+        console.log("Slithering...");
+        _super.prototype.move.call(this, distanceInMeters);
+    };
+    return Rhino;
+}(Animal));
+var Elephant = /** @class */ (function (_super) {
+    __extends(Elephant, _super);
+    function Elephant(name) {
+        return _super.call(this, name) || this;
+    }
+    Elephant.prototype.move = function (distanceInMeters) {
+        if (distanceInMeters === void 0) { distanceInMeters = 20; }
+        console.log("Slithering...");
+        _super.prototype.move.call(this, distanceInMeters);
+    };
+    return Elephant;
+}(Animal));
+var array = [new Rhino('Rinocerator'), new Snake("Serpentina"), new Elepha,
+    nt("Elefanton")];
+var ej2 = { Rhino: new Rhino('Rinocerator'), Snake: new Snake("Serpentina"), Elephant: new Elephant("Elefanton") };
+console.log(array);
--- libro2.ts
+++ libro2.ts
@@ -1,30 +1,33 @@
-var __extends = (this && this.__extends) || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __(
-    ));
-    };
-    var Point = (function () {
-    function Point(x, y) {
-    this.x = x;
-    this.y = y;
+class Animal {
+    name: string;
+    constructor(theName: string) { this.name = theName; }
+    move(distanceInMeters: number = 0) {
+    console.log(`${this.name} moved ${distanceInMeters}m.`);
     }
-    Point.prototype.add = function (point) {
-    return new Point(this.x + point.x, this.y + point.y);
-    };
-    return Point;
-    }());
-    var Point3D = (function (_super) {
-    __extends(Point3D, _super);
-    function Point3D(x, y, z) {
-    _super.call(this, x, y);
-    this.z = z;
     }
-    Point3D.prototype.add = function (point) {
-    var point2D = _super.prototype.add.call(this, point);
-    return new Point3D(point2D.x, point2D.y, this.z + point.z);
-    };
-    return Point3D;
-    }(Point));
-    
-    
+    class Snake extends Animal {
+    constructor(name: string) { super(name); }
+    move(distanceInMeters = 5) {
+    console.log("Slithering...");
+    super.move(distanceInMeters);
+    }
+    }
+    class Rhino extends Animal {
+    constructor(name: string) { super(name); }
+    move(distanceInMeters = 10) {
+    console.log("Slithering...");
+    super.move(distanceInMeters);
+    }
+    }
+    class Elephant extends Animal {
+    constructor(name: string) { super(name); }
+    move(distanceInMeters = 20) {
+    console.log("Slithering...");
+    super.move(distanceInMeters);
+}
+}
+let array : Animal[] = [ new Rhino('Rinocerator'), new Snake("Serpentina"), new Elepha
+nt("Elefanton") ];
+let ej2 = {Rhino: new Rhino('Rinocerator'), Snake: new Snake("Serpentina"), Elephant:
+new Elephant("Elefanton")};
+console.log(array);
