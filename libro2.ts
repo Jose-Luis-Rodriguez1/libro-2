782b88858ef3a9bd96a16becb7b6c07bf0ddb20c
ZarelyNL <zarely_navarrete@hotmail.com>
2019-09-01 23:55:03 -0600

    sobrecarga

--- libro2.ts
+++ libro2.ts
@@ -1,33 +1,6 @@
-class Animal {
-    name: string;
-    constructor(theName: string) { this.name = theName; }
-    move(distanceInMeters: number = 0) {
-    console.log(`${this.name} moved ${distanceInMeters}m.`);
-    }
-    }
-    class Snake extends Animal {
-    constructor(name: string) { super(name); }
-    move(distanceInMeters = 5) {
-    console.log("Slithering...");
-    super.move(distanceInMeters);
-    }
-    }
-    class Rhino extends Animal {
-    constructor(name: string) { super(name); }
-    move(distanceInMeters = 10) {
-    console.log("Slithering...");
-    super.move(distanceInMeters);
-    }
-    }
-    class Elephant extends Animal {
-    constructor(name: string) { super(name); }
-    move(distanceInMeters = 20) {
-    console.log("Slithering...");
-    super.move(distanceInMeters);
+function a(x: number);
+function a(x: String);
+function a(x: boolean);
+function a(x: Array<number>);
+function a(x){
 }
-}
-let array : Animal[] = [ new Rhino('Rinocerator'), new Snake("Serpentina"), new Elepha
-nt("Elefanton") ];
-let ej2 = {Rhino: new Rhino('Rinocerator'), Snake: new Snake("Serpentina"), Elephant:
-new Elephant("Elefanton")};
-console.log(array);
