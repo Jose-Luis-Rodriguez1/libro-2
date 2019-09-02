a8034daf23985d6d08b9cf656de6d0f0e14abd05
ZarelyNL <zarely_navarrete@hotmail.com>
2019-09-01 23:52:49 -0600

    herencia2

--- libro2.js
+++ libro2.js
@@ -1,17 +1,11 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Point = /** @class */ (function () {
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b)
+        if (b.hasOwnProperty(p))
+            d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var Point = (function () {
     function Point(x, y) {
         this.x = x;
         this.y = y;
@@ -21,12 +15,11 @@ var Point = /** @class */ (function () {
     };
     return Point;
 }());
-var Point3D = /** @class */ (function (_super) {
+var Point3D = (function (_super) {
     __extends(Point3D, _super);
     function Point3D(x, y, z) {
-        var _this = _super.call(this, x, y) || this;
-        _this.z = z;
-        return _this;
+        _super.call(this, x, y);
+        this.z = z;
     }
     Point3D.prototype.add = function (point) {
         var point2D = _super.prototype.add.call(this, point);
--- libro2.ts
+++ libro2.ts
@@ -1,23 +1,30 @@
-class Point {
-    x: number;
-    y: number;
-    constructor(x: number, y: number) {
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __(
+    ));
+    };
+    var Point = (function () {
+    function Point(x, y) {
     this.x = x;
     this.y = y;
     }
-    add(point: Point) {
+    Point.prototype.add = function (point) {
     return new Point(this.x + point.x, this.y + point.y);
-    }
-    }
-    class Point3D extends Point {
-    z: number;
-    constructor(x: number, y: number, z: number) {
-    super(x, y);
+    };
+    return Point;
+    }());
+    var Point3D = (function (_super) {
+    __extends(Point3D, _super);
+    function Point3D(x, y, z) {
+    _super.call(this, x, y);
     this.z = z;
     }
-    add(point: Point3D) {
-    var point2D = super.add(point);
+    Point3D.prototype.add = function (point) {
+    var point2D = _super.prototype.add.call(this, point);
     return new Point3D(point2D.x, point2D.y, this.z + point.z);
-    }
-    }
+    };
+    return Point3D;
+    }(Point));
+    
     
