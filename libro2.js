deee0fdd6a87dc8ed5e3728a2875ea0cb30cf006
ZarelyNL <zarely_navarrete@hotmail.com>
2019-09-02 00:00:39 -0600

    classdec

--- libro2.js
+++ libro2.js
@@ -4,42 +4,17 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); }
-};
-function log(constructor) {
-    console.log('Registered Class: ' + constructor['name'] + ' at ' + Date.now());
+function ClassDecoratorParams(param1, param2) {
+    return function (target) {
+        console.log("ClassDecoratorParams(" + param1 + ", '" + param2 + "') called on:", target);
+    };
 }
- > ;
-void {
-    console: console, : .log('Registered Method: ' + propertyKey.toString() + ' at ' + Date.now())
-};
-function logparam(target, propertyKey, parameterIndex) {
-    console.log('Registered Parameter: ' + propertyKey.toString() + ' - ' + parameterI, ndex + ' at ' + Date.now());
-}
-var MyClass = /** @class */ (function () {
-    function MyClass() {
-        this.name = 'name';
-        console.log('constructor');
+var ClassDecoratorParamsExample = /** @class */ (function () {
+    function ClassDecoratorParamsExample() {
     }
-    MyClass.prototype.myMethod = function () {
-        console.log('method');
-    };
-    MyClass.prototype.myMethod2 = function (param1, param2) {
-        console.log('method2');
-    };
-    __decorate([
-        logm
-    ], MyClass.prototype, "myMethod");
-    __decorate([
-        logm,
-        __param(1, logparam)
-    ], MyClass.prototype, "myMethod2");
-    MyClass = __decorate([
-        log
-    ], MyClass);
-    return MyClass;
+    ClassDecoratorParamsExample = __decorate([
+        ClassDecoratorParams(1, "a"),
+        ClassDecoratorParams(2, "b")
+    ], ClassDecoratorParamsExample);
+    return ClassDecoratorParamsExample;
 }());
-var myClass = new MyClass();
-myClass.myMethod();
-myClass.myMethod2(1, false);
